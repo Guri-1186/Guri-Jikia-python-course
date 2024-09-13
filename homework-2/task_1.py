83bc735d95d797868b8fa96b0bee837b13b8fa5d
Guranda Jikia <jikiaguranda@gmail.com>
2024-09-13 21:33:23 +0400

    feat: complete task-1

---
+++ homework-2/task_1.py
@@ -0,0 +1,12 @@
+
+"""
+task.1 Დაწერეთ პროგრამა რომელიც მიიღებს true ან false. 
+Თუ პროგრამამ მიიღო true, ეკრანზე დაბეჭდეთ “whoala”.
+"""
+user_input = input("please entertr true or false :").lower()
+if user_input == "true":
+  print("whoala")
+
+ 
+ 
+ 
