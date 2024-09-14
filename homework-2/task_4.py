df1fc55f1515ddb78b4b4bc2706c51901f879a39
Guranda Jikia <jikiaguranda@gmail.com>
2024-09-14 23:22:02 +0400

    feat:complete task-4

--- homework-2/task_4.py
+++ homework-2/task_4.py
@@ -8,6 +8,28 @@
 მისი კატეგორიაა - VERY FAST.
 Თუ ავტომობილის სიჩქარე მეტია 60 კმ/სთ-ზე,მისი კატეგორიაა - FAST.
 Ხოლო თუ ავტომობილის სიჩქარე მეტია 30 კმ/სთ-ზე, მისიკატეგორიაა - MODERATE.
-(თუ ავტომობლი ხვდება ორ კატეგორაიში, უნდა შეირჩესუფრო სწრაფი კატეგორია)
-
+(თუ ავტომობლი ხვდება ორ კატეგორაიში, უნდა შეირჩეს უფრო სწრაფი კატეგორია)
 """
+#Solution 1
+car_speed = int(input("Please enter the car speed (in km/h) :"))
+
+if car_speed > 120:
+  print("Your car is VERY FAST")
+elif car_speed > 60 and car_speed <= 120:
+  print("Your car is FAST")
+elif car_speed > 30 and car_speed <= 60:
+    print("Your car is MODERATE")
+else:
+  print("Your car is SLOW ")
+
+#Solution 1
+if car_speed > 120:
+  print("Your car is VERY FAST")
+elif car_speed > 60:
+   print("Your car is FAST")
+elif car_speed > 30:
+  print("Your car is MODERATE")
+else:
+  print("Your car is SLOW ")
+  
+  
