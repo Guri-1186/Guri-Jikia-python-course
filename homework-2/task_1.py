c1ea7708f636c4ba91a8479153226062244d11eb
Guranda Jikia <jikiaguranda@gmail.com>
2024-09-17 06:52:33 +0400

    feat:update branch

--- homework-2/task_1.py
+++ homework-2/task_1.py
@@ -3,7 +3,7 @@
 task.1 Დაწერეთ პროგრამა რომელიც მიიღებს true ან false. 
 Თუ პროგრამამ მიიღო true, ეკრანზე დაბეჭდეთ “whoala”.
 """
-user_input = input("please enterter true or false :").lower()
+user_input = input("please enter true or false :").lower()
 if user_input == "true":
   print("whoala")
 else:
