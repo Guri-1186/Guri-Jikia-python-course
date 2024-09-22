060899033e0d5c75632cfb4a8d13299583acd519
Guranda Jikia <jikiaguranda@gmail.com>
2024-09-22 21:06:48 +0400

    feat: complete task_3

--- homework-4/task_3.py
+++ homework-4/task_3.py
@@ -5,6 +5,13 @@
 0 < n < 1000. Მაგალითად:
 Enter number: 18
 1 2 3 6 9 18
-
-
 """
+positive_integer = int(input("Please enter positive integer:"))
+
+if positive_integer <= 0 or positive_integer >= 1000:
+  print("Invalid Input")
+  exit(1)
+
+for num in range(1, positive_integer +1):
+    if positive_integer % num == 0:
+      print(num)
