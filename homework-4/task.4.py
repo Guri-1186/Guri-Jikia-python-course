d3a4bb7b0fa93a1c2b6f55bc72e5a04c6b0655c9
Guranda Jikia <jikiaguranda@gmail.com>
2024-09-22 22:34:42 +0400

    fix: fix task-4

--- homework-4/task.4.py
+++ homework-4/task.4.py
@@ -1,14 +1,11 @@
 
 
 positive_integer = int(input("Please enter positive integer: "))
-
-
 if positive_integer < 0 or positive_integer >= 20:
     print("Invalid input, please enter a non negative integer less than 20.")
     exit(1)
-  
+ 
 num_1, num_2 = 0, 1
-
 for _ in range(positive_integer):
     num_1, num_2 = num_2, num_1 + num_2
 
