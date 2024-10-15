112f67ca8ee07786a5b76dbbc479748f7c4d0c19
Guranda Jikia <jikiaguranda@gmail.com>
2024-10-15 14:05:59 +0400

    feat:complete task-3

--- homework-7/task_3.py
+++ homework-7/task_3.py
@@ -4,3 +4,21 @@
 მაშინ პროგრამამ უნდა დაბეჭდოს 
 შუა ორი სიმბოლო.
 """
+
+while True:
+    word = input("Please enter word :")
+    if word.replace(" ", ""). isalpha():
+        break
+    else:
+         print("Please enter string")
+   
+i = 0
+while i < 5: 
+    length = int(len(word))
+    middle_el = int(length/2)
+    if length % 2 == 0:
+        print(word[middle_el-1:middle_el+1])
+    else:
+        print(word[-1], word[middle_el], word[0])
+    
+    i+=1
