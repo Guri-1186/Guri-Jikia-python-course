2332ba7b232c7d6255c5c27260f3640bd2215593
Guranda Jikia <jikiaguranda@gmail.com>
2024-10-15 13:03:15 +0400

    feat: complete task-2

--- homework-7/task_2.py
+++ homework-7/task_2.py
@@ -2,3 +2,14 @@
 პროგრამამ უნდა შეგვაყვანინოს სიტყვა და დაბეჭდოს ამ 
 სიტყვიდან მხოლოდ თანხმოვანი ასოები.
 """
+while True:
+    user_input = input("Please enter word :")
+    if user_input.replace(" ", "").isalpha():
+        break
+    else:
+        print("Please enter string")
+
+for el in range(0, len(user_input)):
+    if user_input[el].lower()  not in "aeiou":
+        print(user_input[el])
+   
