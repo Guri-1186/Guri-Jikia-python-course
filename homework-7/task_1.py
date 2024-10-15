1fe6777b436a5d334b39e1280f0ca0882883c314
Guranda Jikia <jikiaguranda@gmail.com>
2024-10-15 12:45:58 +0400

    refactor: refactor task-1

--- homework-7/task_1.py
+++ homework-7/task_1.py
@@ -2,11 +2,15 @@
 პროგრამამ უნდა მოგვთხოვოს სტრიქონის შეყვანა. დაბეჭდოს შეყვანილი სტრიქონის ყველა
 ლუწი ინდექსის მქონე სიმბოლო, გარდა "e"- სიმბოლოსი. 
 """
+while True:
+    user_input = input("Please enter string :")
+    if user_input.replace(" ", "").isalpha():
+        break
+    else:
+        print(" Please enter only string")
 
-user_input = input("Please enter string :")
+for el in range(0, len(user_input)):
+    if el % 2 == 0 and user_input[el].lower() not in "e":
+        print(user_input[el])
 
-for el in range(0,len(user_input)):
- if el % 2 == 0 and user_input[el] not in "e":
-    print(user_input[el])
-   
-        
+
