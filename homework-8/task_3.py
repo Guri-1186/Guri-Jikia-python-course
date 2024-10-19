5655f9afef95b273a45479f3a3195a5e82824668
Guranda Jikia <jikiaguranda@gmail.com>
2024-10-19 21:33:58 +0400

    feat:complete tasks

--- homework-8/task_3.py
+++ homework-8/task_3.py
@@ -8,12 +8,17 @@ from datetime import datetime
 
 user_input = input("Please enter date: for.eg (2024-03-22T19:17:42.956376+04:00):").lower()
 
-var_1 = user_input.replace("t", " ").split(".")[0]
+date_part = user_input.replace("t", " ").split(".")[0].split(" ")[0]
+year, month, day = date_part.split("-")
+formated_date = f"{day}-{month}-{year}"
+time_part =user_input.replace("t", " ").split(".")[0].split(" ")[1]
 timezone_part = user_input[-6:-3].replace("0","")
 
-print(var_1, timezone_part)
-# input - Input: 2024-03-04T11:17:42.000123+04:00
-# my output = 2024-03-04 11:17:42 +4
 
-# output - Output: 04-03-2024 11:17:42 +4
+# Input: 2024-03-22T11:17:42.000123+04:00
+# output = 22-03-2024 11:17:42 +4
+print(formated_date,time_part, timezone_part)
+
+
+
 
