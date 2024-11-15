6c29337f067497dbc2937b9dadf76207af5d4cfb
Guranda Jikia <jikiaguranda@gmail.com>
2024-11-15 17:07:57 +0400

    complete tasks

---
+++ homework-15/task_3.py
@@ -0,0 +1,23 @@
+"""
+3. Დაწერეთ პროგრამა რომელიც მიიღებს ინფორმაციას ვის ვინ დაუმეგობრდა. 
+Თუ პროგრამამ მიიღო სიტყვა FINISH, უნდა დაასრულოს მუშაობა.
+Თუ პროგრამამ მიიღო სიტყვა Giorgi – Nini, 
+ნიშნავს რომ Nini დაუმეგობრდა Giorgi-ს და Giorgi დაუმეგობრდა Ninis. 
+Პროგრამამ მუშაობის დასრულების წინ, ეკრანზე უნდა დაბეჭდოს ვინ ვისი მეგობარია. 
+"""
+def get_friends():
+    friends = {} 
+    while True:
+        user_input = input("Enter names (e.g.'Giorgi - Nini') or 'FINISH': ")
+        if user_input == "FINISH":
+            break
+        if " - " in user_input:
+            name1, name2 = user_input.split(" - ")  
+            friends[name1] = name2
+            friends[name2] = name1
+        else:
+            print("Please use format: 'Name1 - Name2'")
+    for person, friend in friends.items():
+        print(f"{person} - {friend}")
+
+get_friends()
